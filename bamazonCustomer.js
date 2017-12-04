2c3c56c609cd3bcb94643fd2668cab5526c88e78
Shannon Berry-Heller <shannonberry-heller@Shannons-MacBook.local>
2017-12-04 13:04:18 -0600

    prompts added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -24,7 +24,35 @@ connection.connect(function(err) {
 function showItems(){
     var query = "SELECT * FROM products";
         connection.query(query, function(err, res){
+        for (var i = 0; i < res.length; i++) {
+          console.log("ID: " + res[i].item_id + " || Product: " + res[i].product_name + " || Department: " + res[i].department_name + " || Price: " + res[i].price);
+        }
           console.log(err);
-          console.log(res);
+          buyItem();
+
         });
-};
+    };
+
+// function to handle posting new items up for auction
+function buyItem() {
+  // prompt for info about the item being put up for auction
+  inquirer
+    .prompt([
+      {
+        name: "item",
+        type: "input",
+        message: "What is the ID of the product you would like to purchase?",
+        validate: function(value) {
+            if (isNaN(value) === false) {
+            return true;
+          }
+            return false;
+        },
+      },
+      {
+        name: "category",
+        type: "input",
+        message: "How many units of the product would you like?"
+      }
+    ])
+  };
